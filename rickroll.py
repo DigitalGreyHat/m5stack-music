3ec9da83dedc36937b59a3a17770188e04291319
Digital Grey Hat <90456651+DigitalGreyHat@users.noreply.github.com>
2022-03-30 15:41:08 +0200

    Update rickroll.py

--- rickroll.py
+++ rickroll.py
@@ -194,8 +194,8 @@ for thisNote in range (0, notes*2, 2):
     elif divider < 0:
         noteDuration = (wholenote) / abs(divider)
         noteDuration = noteDuration * 1.5
-
-    speaker.tone(int(melody[thisNote]), int(noteDuration))
+    if int(melody[thisNote]) != 0:
+        speaker.tone(int(melody[thisNote]), int(noteDuration))
     breakDuration = int((noteDuration) * 0.1)
     wait_ms(int(breakDuration) - 5)
 
